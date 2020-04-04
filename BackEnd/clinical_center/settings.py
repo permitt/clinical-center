edd87077c6ff501c60a7912d9bd0af34a34b9a28
Mitar Perovic <perovicmitar@gmail.com>
2020-04-04 15:48:21 +0200

    Added django-cors-headers

--- BackEnd/clinical_center/settings.py
+++ BackEnd/clinical_center/settings.py
@@ -37,11 +37,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # custom apps
+    'users',
+
+    # 3rd party
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
