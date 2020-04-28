17e834b973b9e10c5e7bfd2b27b58398a6429ac6
Mitar Perovic <perovicmitar@gmail.com>
2020-04-28 13:10:56 +0200

    Created app clinics, created models for the clinic, appointment, operatingRoom, ratings...

---
+++ backend/clinics/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+from users.models import Doctor,Patient
+
+class Clinic(models.Model):
+    name = models.CharField(max_length=30)
+    address = models.CharField(max_length=30)
+    city = models.CharField(max_length=30)
+    country = models.CharField(max_length=30)
+    descripion = models.TextField()
+    #availableTerms - should just check the reserved dateTimes
+
+    class Meta:
+        ordering = ['name','city','country']
+
+class OperatingRoom(models.Model):
+    clinic = models.ForeignKey(to=Clinic, on_delete=models.CASCADE, related_name='operating_rooms')
+    name = models.CharField(max_length=20)
+    number = models.IntegerField()
+
+
+class PriceList(models.Model):
+    clinic = models.ForeignKey(to=Clinic, on_delete=models.CASCADE, related_name='prices')
+    appointmentType = models.CharField(unique=True)
+    price = models.FloatField()
+
+class Appointment(models.Model):
+    clinic = models.ForeignKey(to=Clinic, on_delete=models.CASCADE, related_name='appointments')
+    dateTime = models.DateTimeField()
+    typeOf = models.CharField(max_length=30)
+    #price from .clinic.prices
+    discount = models.IntegerField(default=0)
+    doctor = models.ForeignKey(to=Doctor, on_delete=models.CASCADE, related_name='appointments')
+    operatingRoom = models.ForeignKey(to=OperatingRoom, on_delete=models.CASCADE, related_name='appointments')
+    patient = models.ForeignKey(to=Patient, on_delete=models.CASCADE, related_name='appointments', null=True)
+
+class Ratings(models.IntegerChoices):
+    ONE = 1
+    TWO = 2
+    THREE = 3
+    FOUR = 4
+    FIVE = 5
+
+class DoctorRating(models.Model):
+    doctor = models.ForeignKey(to=Doctor, on_delete=models.CASCADE)
+    patient = models.ForeignKey(to=Patient, on_delete=models.CASCADE)
+    rating = models.IntegerField(choices=Ratings.choices)
+
+class ClinicRating(models.Model):
+    clinic = models.ForeignKey(to=Clinic, on_delete=models.CASCADE)
+    patient = models.ForeignKey(to=Patient, on_delete=models.CASCADE)
+    rating = models.IntegerField(choices=Ratings.choices)
