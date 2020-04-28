eab77488081396f01a871d79bae5c4a361cee390
Mitar Perovic <perovicmitar@gmail.com>
2020-04-28 14:45:08 +0200

    Register clinics app and configure urls.py

--- backend/clinical_center/urls.py
+++ backend/clinical_center/urls.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('api/', include('users.urls')),
+    path('api/users/', include('users.urls')),
+    path('api/clinics/', include('clinics.urls')),
     path('admin/', admin.site.urls),
 ]
