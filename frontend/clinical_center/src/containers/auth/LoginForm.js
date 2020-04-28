362329e154658bd656c64001c426c850c8690f31
specnazm <laketic.milena98@gmail.com>
2020-04-28 10:19:57 +0200

    Added alert message if credentials are not valid

--- frontend/clinical_center/src/containers/auth/LoginForm.js
+++ frontend/clinical_center/src/containers/auth/LoginForm.js
@@ -4,8 +4,10 @@ import { withRouter } from 'react-router-dom'
 import { Formik, Form, Field } from 'formik'
 import Grid from '@material-ui/core/Grid'
 import TextField from '@material-ui/core/TextField'
-import { logIn } from '../../store/actions/AuthActions'
 import Button from '@material-ui/core/Button'
+import Alert from '@material-ui/lab/Alert';
+
+import { logIn } from '../../store/actions/AuthActions'
 import { loginSchema } from './validations'
 import { formStyle, submitButton } from '../../styles/FormStyle'
 import { withFormikField } from '../../utils/withFormikField'
@@ -54,6 +56,7 @@ class LoginForm extends Component {
               />
             </Grid>
           </Grid>
+        {this.props.loginError && <Alert severity="error" style={{marginTop:'10px'}}>Username or password not valid!</Alert>}
           <Button
             type="submit"
             fullWidth
